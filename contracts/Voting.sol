e62e34dbd0828db30d41e27dc545c57fe3bd6d5f
joepike <joseph.l.pike@gmail.com>
2018-01-28 12:58:43 +0000

    finalized project

---
+++ contracts/Voting.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.4.18;
+// We have to specify what version of compiler this code will compile with
+
+contract Voting {
+
+  struct Voter {
+    bool voted;
+    bool registered;
+  }
+
+  event Registered(address voter);
+
+  mapping(address => Voter) public voters;
+
+  /* mapping field below is equivalent to an associative array or hash.
+  The key of the mapping is candidate name stored as type bytes32 and value is
+  an unsigned integer to store the vote count
+  */
+
+  mapping (bytes32 => uint8) public votesReceived;
+
+  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
+  We will use an array of bytes32 instead to store the list of candidates
+  */
+
+  bytes32[] public candidateList;
+
+  /* This is the constructor which will be called once when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+    require(validCandidate(candidate));
+    return votesReceived[candidate];
+  }
+
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForCandidate(bytes32 candidate) public registeredVoter voterNotVoted {
+    Voter storage currentVoter = voters[msg.sender];
+    require(!currentVoter.voted);
+    require(validCandidate(candidate));
+
+    currentVoter.voted = true;
+    votesReceived[candidate] += 1;
+  }
+
+  function validCandidate(bytes32 candidate) view public returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /*OUR NEW FUNCTIONS BELOW */
+  function registerVoter(address account) public {
+    Voter storage newVoter = voters[account];
+    newVoter.registered = true;
+    Registered(account);
+  }
+
+  modifier registeredVoter() {
+    require(voters[msg.sender].registered);
+    _;
+  }
+
+  modifier voterNotVoted() {
+    require(!voters[msg.sender].voted);
+    _;
+  }
+}
